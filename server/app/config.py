2f67aa4338dc80546300229ed6e77306e96673f1
Gideon <kamaugedion95@gmail.com>
2025-04-25 20:43:54 +0300

    feat: load email settings from .env and update app factory for email support

--- server/app/config.py
+++ server/app/config.py
@@ -11,12 +11,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
-    #MAIL_SERVER = 'smtp.gmail.com'
-    #MAIL_PORT = 587
-    #MAIL_USE_TLS = True
-    #MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    #MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
+    
 
     
 
