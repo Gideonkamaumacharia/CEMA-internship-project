f68582001aab262dfe9c14729c550a5fff6140f2
Gideon <kamaugedion95@gmail.com>
2025-04-26 23:15:24 +0300

    feat: add is_admin field to Doctor model and create corresponding migration

--- server/app/models.py
+++ server/app/models.py
@@ -10,6 +10,7 @@ class Doctor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
     
 
     # relationships
