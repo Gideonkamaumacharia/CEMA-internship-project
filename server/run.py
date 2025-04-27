746d391b345743bb0eb8c95ff3d7e5fce7efe97e
Gideon <kamaugedion95@gmail.com>
2025-04-27 14:38:01 +0300

    Disabled debug mode for production

--- server/run.py
+++ server/run.py
@@ -4,5 +4,6 @@ from app import create_app  # Import the app creation function from the app modu
 app = create_app()
 
 if __name__ == "__main__":
-    # Run the app in debug mode (for development)
-    app.run(debug=True)
+   
+    # Set debug=True to enable debug mode
+    app.run()
